43565e9505c4d53c2cb41cd68dc26ef161f4e9d6
Nikita Barantsev <nbarentsev@gmail.com>
2022-11-24 22:27:18 +0700

    add config

---
+++ lib/config/config.dart
@@ -0,0 +1,5 @@
+class Config {
+  static const host = 'https://api.themoviedb.org/3';
+  static const imageUrl = 'https://image.tmdb.org/t/p/w500';
+  static const apiKey = '4fd63fa3d9fdc3aeac618d158f149890';
+}
