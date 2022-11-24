8073eb8d19bcce52671102ee814048d88777e2d8
Nikita Barantsev <nbarentsev@gmail.com>
2022-11-24 18:26:35 +0700

    move my app

---
+++ lib/ui/widgets/app/my_app.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_themoviedb/ui/navigation/main_navigation.dart';
+import 'package:flutter_themoviedb/ui/theme/app_colors.dart';
+
+class MyApp extends StatelessWidget {
+  static final mainNavigation = MainNavigation();
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.mainDarkBlue,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: AppColors.mainDarkBlue,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+        ),
+      ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ru', 'RU'), // Russian
+        Locale('en', ''), // English
+      ],
+      routes: mainNavigation.routes,
+      initialRoute: MainNavigationRouteNames.loaderWidget,
+      onGenerateRoute: mainNavigation.onGenerateRoute,
+    );
+  }
+}
